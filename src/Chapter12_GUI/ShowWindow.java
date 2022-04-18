3f82cdaaecee44de2937dc4e8f3bea8166b1bd30
Chris Aguirre <caguirr7@montgomerycollege.edu>
2022-04-17 20:54:45 -0400

    Show Window

---
+++ src/Chapter12_GUI/ShowWindow.java
@@ -0,0 +1,26 @@
+package Chapter12_GUI;
+
+import javax.swing.*;
+
+public class ShowWindow {
+    public static void main(String[] args){
+            final int WINDOW_WIDTH = 350; //Window width in pixels
+            final int WINDOW_HEIGHT = 250; // Window height in pixels
+
+        //Create the title
+        JFrame window = new JFrame();
+
+        //Set the title
+        window.setTitle("A simple window");
+
+        //Set the size of the window
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        //Specify what happens when the close button is clicked
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Display the window
+        window.setVisible(true);
+
+    }
+}
